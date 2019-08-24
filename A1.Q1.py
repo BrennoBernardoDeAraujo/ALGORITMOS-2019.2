e12042e9c683b09f5b5a7f242624ef39a073b212
BrennoBernardoDeAraujo <54484775+BrennoBernardoDeAraujo@users.noreply.github.com>
2019-08-24 19:52:25 -0300

    Add files via upload

---
+++ A1.Q1.py
@@ -0,0 +1,4 @@
+VdoLCOMB = float(input("Valor do litro de combustível: "))
+VaABAST = float(input("Valor que deseja abastecer: "))
+QdeL = float(VaABAST / VdoLCOMB)
+print ("Quantidade de litros abastecidos: ",QdeL)
