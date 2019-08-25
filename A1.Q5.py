37afe4396617c63a46075f2e21856de80f3fec65
BrennoBernardoDeAraujo <54484775+BrennoBernardoDeAraujo@users.noreply.github.com>
2019-08-24 22:40:01 -0300

    Add files via upload

---
+++ A1.Q5.py
@@ -0,0 +1,4 @@
+CD = float(input("Cotação do dólar: "))
+QR = float(input("Quanto em reais deseja converter: "))
+QD = float QR/CD
+print ("A quantidade de reais informados equivalem em dólares a: ",QD)
