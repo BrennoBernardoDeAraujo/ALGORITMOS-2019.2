d7d7feaab377cc610d0cc8a2760d242e14fba315
BrennoBernardoDeAraujo <54484775+BrennoBernardoDeAraujo@users.noreply.github.com>
2019-08-24 23:08:35 -0300

    Add files via upload

---
+++ A1.Q7.py
@@ -0,0 +1,4 @@
+SM = float (input("Salário mensal: "))
+DM = float(input("Despesas mensais: "))
+AnosQF = float (1000000/(SM - DM))/12
+print ("o tempo (em anos) que  irá demorar para poupar Um Milhão de Reais: ", AnosQF))
