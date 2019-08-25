46e83d8097881a8241404ccdae8a4c2849ac0fd6
BrennoBernardoDeAraujo <54484775+BrennoBernardoDeAraujo@users.noreply.github.com>
2019-08-24 21:31:10 -0300

    Add files via upload

---
+++ A1.Q4.py
@@ -0,0 +1,14 @@
+CC1 = float(input("Comprimento do cômodo 1: "))
+LC1 = float(input("lagura do cômodo 1: "))
+CC2 = float(input("Comprimento do cômodo 2: "))
+LC2 = float(input("lagura do cômodo 2: "))
+CC3 = float(input("Comprimento do cômodo 3: "))
+LC3 = float(input("lagura do cômodo 3: "))
+CC4 = float(input("Comprimento do cômodo 4: "))
+LC4 = float(input("lagura do cômodo 4: "))
+C1 =  float CC1 * LC1
+C2 =  float CC2 * LC2
+C3 =  float CC3 * LC3
+C4 =  float CC4 * LC4
+AT =  float C1 + C2 + C3 + C4
+print ("Área total da casa: ",AT)
