e60e87d3fb882670477819a30ecb412ea0d4d9ba
BrennoBernardoDeAraujo <54484775+BrennoBernardoDeAraujo@users.noreply.github.com>
2019-08-25 17:05:12 -0300

    Add files via upload

---
+++ A1.Q1 melhorado.py
@@ -0,0 +1,14 @@
+# 1. Faça um programa que solicite ao usuário o valor do litro de combustível (ex. 4,75) e
+# quanto em dinheiro ele deseja abastecer (ex. 50,00). Calcule quantos litros de combustível o
+# usuário obterá com esses valores.
+
+VL = float(input("Valor do litro de combustível: "))
+VA = float(input("Valor que deseja abastecer: "))
+QL = float(VA / VL)
+print ("Quantidade de litros abastecidos: ",QL)
+
+# Siginificado das siglas:
+
+# VL = float(input("Valor do litro de combustível: "))
+# VA = float(input("Valor que deseja abastecer: "))
+# QL = Quantidade de litros abastecidos
