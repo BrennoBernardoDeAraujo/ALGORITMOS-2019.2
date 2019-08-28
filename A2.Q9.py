451c53ef3bc19dd5bf8d2e1aaa654fe22826f8b0
BrennoBernardoDeAraujo <54484775+BrennoBernardoDeAraujo@users.noreply.github.com>
2019-08-27 23:14:16 -0300

    Add files via upload

---
+++ A2.Q9.py
@@ -0,0 +1,29 @@
+# 9.Faça um Programa que leia um número e exiba o dia correspondente da semana. 
+# (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.
+
+
+DIA = int(input('Digite um número de (1 a 7): '))
+
+if DIA == 1:
+    print(' Segunda')
+
+elif DIA == 2:
+    print(' Terça')
+
+elif DIA == 3:
+    print(' Quarta')
+
+elif DIA == 4:
+    print(' Quinta')
+
+elif DIA == 5:
+    print(' Sexta')
+
+elif dia da semana == 6:
+    print(' Sabado')
+
+elif DIA == 7:
+    print(' Domingo')
+
+else:
+    print('valor inválido')
