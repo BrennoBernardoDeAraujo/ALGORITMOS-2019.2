32999681f6a801454ac8da730f5103a5c457922f
BrennoBernardoDeAraujo <54484775+BrennoBernardoDeAraujo@users.noreply.github.com>
2019-08-31 22:58:53 -0300

    Add files via upload

---
+++ A2.Q7.py
@@ -0,0 +1,17 @@
+# 7.Faça um Programa que pergunte em que turno você estuda. 
+# Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. 
+# Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+
+turno = input('Digite em que turno você estuda: M=maturnino, V=vespertino ou N-noturno: ')
+
+if turno == 'M':
+    print('Bom dia!')
+
+elif turno == 'V':
+    print('Boa tarde!')
+
+elif turno == 'N':
+    print('Boa noite!')
+
+else:
+    print('Valor inválido!')
