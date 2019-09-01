7be7a2449f67c6207635276f8d94f0b6acbbea1d
BrennoBernardoDeAraujo <54484775+BrennoBernardoDeAraujo@users.noreply.github.com>
2019-09-01 16:14:46 -0300

    Add files via upload

--- A2.Q13.py
+++ A2.Q13.py
@@ -17,30 +17,31 @@ notas50 = 0
 notas10 = dezena
 notas5 = 0
 notas1 = unidade
-
+if Vsaque < 10 or Vsaque > 600:
+    print("valor nao permitido,valor minimo 10 e maximo de 600")
 if dezena > 5:
     notas50 =  1  
     notas10 = dezena - 5
-    if unidade > 5:
+    if unidade > 5 and (Vsaque >= 10 or Vsaque <= 600):
         notas5 =  1 
         notas1 = unidade - 5
-    if notas100 > 1:
+    if notas100 > 1 and (Vsaque >= 10 or Vsaque <= 600):
         print(notas100,"notas de 100 ,",end="")
-    if notas100 == 1:
+    if notas100 == 1 and (Vsaque >= 10 or Vsaque <= 600):
         print(notas100,"nota de 100 ,",end="")
-    if notas50 > 1:
+    if notas50 > 1 and (Vsaque >= 10 or Vsaque <= 600):
         print(notas50,"notas de 50 ,",end="")
-    if notas50 == 1:
+    if notas50 == 1 and (Vsaque >= 10 or Vsaque <= 600):
         print(notas50,"nota de 50 ,",end="")
-    if notas10 > 1:
+    if notas10 > 1 and (Vsaque >= 10 or Vsaque <= 600):
         print(notas10,"notas de 10 ,",end="")
-    if notas10 == 1:
+    if notas10 == 1 and (Vsaque >= 10 or Vsaque <= 600):
         print(notas10,"nota de 10 ",end="")
-    if notas5 > 1:
+    if notas5 > 1 and (Vsaque >= 10 or Vsaque <= 600):
         print(notas5,"notas de 5 ,",end="")
-    if notas5 == 1:
+    if notas5 == 1 and (Vsaque >= 10 or Vsaque <= 600):
         print(notas5,"nota de 5 ,",end="")
-    if notas1 >= 1:
+    if notas1 >= 1 and (Vsaque >= 10 or Vsaque <= 600):
         print(notas1,"notas de 1 .",end="")
-    if notas10 == 1:
+    if notas10 == 1 and (Vsaque >= 10 or Vsaque <= 600):
         print(notas1,"nota de 1 .",end="")
