ed78dcce37a8395ccb36392ff21f1728a6248939
BrennoBernardoDeAraujo <54484775+BrennoBernardoDeAraujo@users.noreply.github.com>
2019-09-01 00:18:50 -0300

    Add files via upload

--- A2.Q13.py
+++ A2.Q13.py
@@ -1,7 +1,17 @@
-numero = int(input(": "))
-centena = int(numero/100)
-dezena = int((numero-(centena*100))/10)
-unidade = int(numero - (centena*100 + dezena*10))
+# 13.Faça um Programa para um caixa eletrônico. O programa deverá perguntar ao usuário o valor do saque
+# e depois informar quantas notas de cada valor serão fornecidas.
+# As notas disponíveis serão as de 1, 5, 10, 50 e 100 reais. 
+# O valor mínimo é de 10 reais e o máximo de 600 reais.
+# O programa não deve se preocupar com a quantidade de notas existentes na máquina.
+# Exemplo 1: Para sacar a quantia de 256 reais, o programa fornece 
+# duas notas de 100, uma nota de 50, uma nota de 5 e uma nota de 1;
+# Exemplo 2: Para sacar a quantia de 399 reais, o programa fornece 
+# três notas de 100, uma nota de 50, quatro notas de 10, uma nota de 5 e quatro notas de 1.
+
+Vsaque = int(input("Valor do saque: "))
+centena = int(Vsaque/100)
+dezena = int((Vsaque -(centena*100))/10)
+unidade = int(Vsaque - (centena*100 + dezena*10))
 notas100 = centena
 notas50 = 0
 notas10 = dezena
@@ -29,8 +39,8 @@ if dezena > 5:
     if notas5 > 1:
         print(notas5,"notas de 5 ,",end="")
     if notas5 == 1:
-        print(notas5,"nota de 10 ,",end="")
+        print(notas5,"nota de 5 ,",end="")
     if notas1 >= 1:
         print(notas1,"notas de 1 .",end="")
     if notas10 == 1:
-        print(notas1,"nota de 10 .",end="")
+        print(notas1,"nota de 1 .",end="")
