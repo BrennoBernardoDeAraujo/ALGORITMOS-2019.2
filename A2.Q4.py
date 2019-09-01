6d2bb934359514f4f72a7374da9df09c855319fa
BrennoBernardoDeAraujo <54484775+BrennoBernardoDeAraujo@users.noreply.github.com>
2019-08-31 22:42:56 -0300

    Add files via upload

---
+++ A2.Q4.py
@@ -0,0 +1,24 @@
+n1 = float(input("digite um numero: "))
+n2 = float(input("digite outro numero: "))
+n3 = float(input("digite mais outro numero: "))
+
+if n1 <= n2 and n2 <= n3:
+    print(n3,n2,n1)
+    if n2 < n1:
+        aux = n1
+        n1 = n2
+        n3 = aux
+    if n3 < n2:
+        aux = n3
+        n3 = n2
+        n2 = aux
+    if n3 < n2:
+        aux = n3
+        n3 = n2
+        n2 = aux
+    if n2 < n1:
+        aux = n1
+        n1 = n2
+        n2 = aux
+print("O maior número é:",n3)
+print("O menor numro é:",n1)
