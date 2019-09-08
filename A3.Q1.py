2d56b06da4687f1d878748c3dfd0db7ee96324fa
BrennoBernardoDeAraujo <54484775+BrennoBernardoDeAraujo@users.noreply.github.com>
2019-09-08 18:13:48 -0300

    Update A3.Q1.py

--- A3.Q1.py
+++ A3.Q1.py
@@ -1,3 +1,6 @@
+# 1.Faça um programa que peça uma nota, entre zero e dez. 
+# Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+
 nota=float(input("informe um numero de 0 a 10: "))
 while (nota>10) or (nota<0):
 	nota=float(input("informe um numero de 0 a 10: "))
