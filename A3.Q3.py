8b4083ffb8642ada7fc22865fb28e3f168b51c7c
BrennoBernardoDeAraujo <54484775+BrennoBernardoDeAraujo@users.noreply.github.com>
2019-09-08 19:13:15 -0300

    Add files via upload

---
+++ A3.Q3.py
@@ -0,0 +1,16 @@
+# 3. Supondo que a população de um país A seja da ordem de 80.000 habitantes com uma taxa 
+# anual de crescimento de 3% \e que a população de B seja 200.000 habitantes com uma taxa de 
+# crescimento de 1.5%. Faça um programa que calcule e escreva o número de anos 
+# necessários para que a população do país A ultrapasse ou iguale a população do país B, 
+# mantidas as taxas de crescimento.
+
+a = 80000
+b = 200000
+ano = 0
+
+while a <= b:
+	a += a * 0.03
+	b += b * 0.015
+	ano += 1
+
+print ( "A populaçao de A vai ultrapassa ou iguala a de B em", ano )
