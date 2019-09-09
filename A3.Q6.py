392a285ee6d6185a4ee1a4471c014103f69b2749
BrennoBernardoDeAraujo <54484775+BrennoBernardoDeAraujo@users.noreply.github.com>
2019-09-09 00:00:21 -0300

    Add files via upload

---
+++ A3.Q6.py
@@ -0,0 +1,14 @@
+num1=float(input("digite o 1º numero--> "))
+num2=float(input("digite o 2º numero--> "))
+num3=float(input("digite o 3º numero--> "))
+num4=float(input("digite o 4º numero--> "))
+num5=float(input("digite o 5º numero--> "))
+num6=float(input("digite o 1º numero--> "))
+num7=float(input("digite o 2º numero--> "))
+num8=float(input("digite o 3º numero--> "))
+num9=float(input("digite o 4º numero--> "))
+num10=float(input("digite o 5º numero--> "))
+
+soma=num1+num2+num3+num4+num5+num6+num7+num8+num9+num10
+print("soma-->",soma,)
+print("média-->",soma/5)
